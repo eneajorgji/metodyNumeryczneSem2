8256a4643ca390f7862f2f5a1adba4e62b7bdd42
Enea Jorgji <jorgji.enea@gmail.cm>
2021-04-03 00:32:46 +0200

    zadanie 2

--- 123991_lab1.py
+++ 123991_lab1.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#########################################
 # Zadanie 1
 
 def f(x):
@@ -30,6 +31,7 @@ def bisection_method(f, a, b, eps=10 ** -6, max_iter=100):
 print(bisection_method(f, 1.5, 3, eps=10 ** -6))
 
 
+#########################################
 # Zadanie 1 + wizualizacja
 
 def bisection_method_vis(f, a, b, eps=10 ** -4, max_iter=100):
@@ -59,3 +61,36 @@ plt.plot(x, f(x))
 plt.axvline(answer)
 plt.grid()
 plt.show()
+
+
+#########################################
+# Zadanie 2
+def f2(x):
+    return (x - 2) ** 2 - 1
+
+
+def golden_section(f, a, b, eps=10 ** -4, max_iter=100):
+    k = (5 ** 0.5 - 1) / 2
+
+    count_iters = 0
+    # x_left = b - (b - a) * k
+    # x_right = a + (b - a) * k
+
+    while abs(b - a) > eps and count_iters < max_iter:
+        count_iters += 1
+
+        x_left = b - (b - a) * k
+        x_right = a + (b - a) * k
+
+        if f(x_left) < f(x_right):
+            b = x_right
+        else:
+            a = x_left
+
+        c = (x_left + x_right) / 2
+        print(count_iters, c)
+
+    return c
+
+
+print(golden_section(f2, 0, 5))
--- lab_1.py
+++ lab_1.py
@@ -8,7 +8,7 @@ def f2(x):
     return (x - 2) ** 2 - 1
 
 
-def golden_section(f, a, b, eps=10 ** -4, max_iter=100):
+def golden_section(f2, a, b, eps=10 ** -4, max_iter=100):
     k = (5 ** .5 - 1) / 2
 
     x_left = b - (b - a) * k
@@ -16,19 +16,19 @@ def golden_section(f, a, b, eps=10 ** -4, max_iter=100):
 
     number_iter = 0
 
-    while (b - a) > eps:
+    while abs(b - a) > eps:
         if f2(x_left < f2(x_right)):
             b = x_right
-            x_right = x_left
-            x_left = b - (b - a) * k
-
-
+            # x_right = x_left
+            # x_left = b - (b - a) * k
         else:
             a = x_left
-            x_left = x_right
-            x_right = a + (b - a) * k
+            # x_left = x_right
+            # x_right = a + (b - a) * k
 
     return (a + b) / 2
+
+
 
 
 answer = golden_section(f2, 0, 5)
